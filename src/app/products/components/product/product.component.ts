be48a80cbc7336dbd8b6ba6d17d7f0b2ebd86fb8
Abdel Raheem <AbdelRaheemSakr@gmail.com>
2023-09-21 12:20:23 +0300

    add models to project

--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product',
@@ -6,7 +7,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent {
-  @Input() product: any = {};
+  @Input() product!: Product;
   @Output() item = new EventEmitter();
   isAddButton2: boolean = false;
   amount: number = 0;
